156c4443b5c42a0e01ea604e7de1d029e7981348
Talgat Muratbekov <talgat2307@gmail.com>
2021-03-26 17:12:00 +0600

    Added global layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
+import { Switch, Route } from 'react-router-dom';
+import Layout from './components/GlobalLayout/Layout';
+import Login from './containers/Authentication/Login';
+import Register from './containers/Authentication/Register';
+import { Redirect } from 'react-router';
+import { useSelector } from 'react-redux';
+
+const ProtectedRoute = ({ isAllowed, redirectTo, ...props }) => {
+  return isAllowed ? <Route {...props}/> : <Redirect to={redirectTo}/>;
+};
 
 const App = () => {
+  const user = useSelector(state => state.userLogin.userInfo);
 
   return (
     <>
-
+      <Layout>
+        <Switch>
+          <ProtectedRoute path={'/register'} component={Register} isAllowed={!user} redirectTo={'/'}/>
+          <Route path={'/login'} component={Login}/>
+        </Switch>
+      </Layout>
     </>
   );
 };
