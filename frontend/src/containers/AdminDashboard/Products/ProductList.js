e4ad01ae504ffc8d3510bb9a9303b56c367fda32
Talgat Muratbekov <talgat2307@gmail.com>
2021-04-12 18:31:23 +0600

    Added Product list & Product create & Product edit pages

---
+++ frontend/src/containers/AdminDashboard/Products/ProductList.js
@@ -0,0 +1,114 @@
+import React, { useEffect } from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Loader from '../../../components/Loader';
+import { Alert } from '@material-ui/lab';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import { DataGrid } from '@material-ui/data-grid';
+import {
+  deleteProduct, getProductList,
+} from '../../../store/actions/productActions';
+
+const useStyles = makeStyles((theme) => ({
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  tableCon: {
+    marginTop: theme.spacing(4),
+  },
+  table: {
+    minWidth: 750,
+    border: '1px solid #00000',
+  },
+}));
+
+const ProductList = () => {
+
+  const dispatch = useDispatch();
+  const { products, loading, error } = useSelector(state => state.productList);
+
+  useEffect(() => {
+    dispatch(getProductList());
+  }, [dispatch]);
+
+  const productDeleteHandler = (id) => {
+    dispatch(deleteProduct(id));
+  };
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'name', headerName: 'NAME', width: 270 },
+    { field: 'price', headerName: 'PRICE', width: 120 },
+    { field: 'category', headerName: 'CATEGORY', width: 120 },
+    { field: 'brand', headerName: 'BRAND', width: 120 },
+    {
+      field: 'CONTROLLERS',
+      width: 170,
+      renderCell: (params) => (
+        <>
+          <Button
+            color={'primary'}
+            component={Link}
+            to={`product/${params.row.productId}/edit`}
+          >
+            <EditIcon/>
+          </Button>
+          <Button
+            color={'secondary'}
+            onClick={() => productDeleteHandler(params.row.productId)}
+          >
+            <DeleteIcon/>
+          </Button>
+        </>
+      ),
+    },
+  ];
+
+  const rows = products.map((product, i) => ({
+    id: i + 1,
+    productId: product._id,
+    name: product.name,
+    price: '$' + product.price,
+    category: product.category,
+    brand: product.brand,
+  }));
+
+  const classes = useStyles();
+  return (
+    <Container>
+      <div className={classes.header}>
+        <Typography variant="h5">
+          Product List
+        </Typography>
+        <Button
+          component={Link}
+          to={'/admin/product-create'}
+          variant={'outlined'}
+          color={'primary'}
+        >
+          + Create Product
+        </Button>
+      </div>
+      {loading ? <Loader open={loading}/> : error ? <Alert severity={'error'}>{error}</Alert> : (
+        <DataGrid
+          className={classes.tableCon}
+          pageSize={8}
+          autoHeight
+          rows={rows}
+          columns={columns.map(column => ({
+            ...column,
+            disableClickEventBubbling: true,
+          }))}
+        />
+      )}
+    </Container>
+  );
+};
+
+export default ProductList;
