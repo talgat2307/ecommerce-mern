b06cc3fd7521f0935ba9cf01258d6274cbeb6097
Talgat Muratbekov <talgat2307@gmail.com>
2021-04-12 18:26:47 +0600

    Added user actions & reducers for admin

--- frontend/src/store/actions/userActions.js
+++ frontend/src/store/actions/userActions.js
@@ -6,6 +6,21 @@ import {
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
   USER_REGISTER_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_FAIL,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCESS,
+  USER_UPDATE_PROFILE_FAIL,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
+  USER_LIST_FAIL,
+  USER_DELETE_REQUEST,
+  USER_DELETE_SUCCESS,
+  USER_DELETE_FAIL,
+  USER_UPDATE_REQUEST,
+  USER_UPDATE_SUCCESS,
+  USER_UPDATE_FAIL, USER_PROFILE_REQUEST, USER_PROFILE_SUCCESS, USER_PROFILE_FAIL,
 } from '../constants/userConstants';
 import axiosApi from '../../axiosApi';
 
@@ -16,6 +31,30 @@ const userLoginFail = error => ({ type: USER_LOGIN_FAIL, error });
 const userRegisterRequest = () => ({ type: USER_REGISTER_REQUEST });
 const userRegisterSuccess = user => ({ type: USER_REGISTER_SUCCESS, user });
 const userRegisterFail = error => ({ type: USER_REGISTER_FAIL, error });
+
+const userProfileRequest = () => ({ type: USER_PROFILE_REQUEST });
+const userProfileSuccess = user => ({ type: USER_PROFILE_SUCCESS, user });
+const userProfileFail = error => ({ type: USER_PROFILE_FAIL, error });
+
+const userUpdateProfileRequest = () => ({ type: USER_UPDATE_PROFILE_REQUEST });
+const userUpdateProfileSuccess = updatedUser => ({ type: USER_UPDATE_PROFILE_SUCCESS, updatedUser });
+const userUpdateProfileFail = error => ({ type: USER_UPDATE_PROFILE_FAIL, error });
+
+const userListRequest = () => ({ type: USER_LIST_REQUEST });
+const userListSuccess = users => ({ type: USER_LIST_SUCCESS, users });
+const userListFail = error => ({ type: USER_LIST_FAIL, error });
+
+const userDeleteRequest = () => ({ type: USER_DELETE_REQUEST });
+const userDeleteSuccess = user => ({ type: USER_DELETE_SUCCESS, user });
+const userDeleteFail = error => ({ type: USER_DELETE_FAIL, error });
+
+const userDetailsRequest = () => ({ type: USER_DETAILS_REQUEST });
+const userDetailsSuccess = user => ({ type: USER_DETAILS_SUCCESS, user });
+const userDetailsFail = error => ({ type: USER_DETAILS_FAIL, error });
+
+const userUpdateRequest = () => ({ type: USER_UPDATE_REQUEST });
+const userUpdateSuccess = updatedUser => ({ type: USER_UPDATE_SUCCESS, updatedUser });
+const userUpdateFail = error => ({ type: USER_UPDATE_FAIL, error });
 
 export const register = (user) => {
   return async dispatch => {
@@ -55,5 +94,93 @@ export const logout = () => {
     dispatch({ type: USER_LOGOUT });
   };
 };
+
+export const getProfile = () => {
+  return async dispatch => {
+    dispatch(userProfileRequest());
+    try {
+      const response = await axiosApi('/users/profile');
+      dispatch(userProfileSuccess(response.data));
+    } catch (e) {
+      dispatch(userProfileFail(e.response && e.response.data
+        ? e.response.data
+        : e.message));
+    }
+  };
+};
+
+export const updateProfile = (user) => {
+  return async dispatch => {
+    dispatch(userUpdateProfileRequest());
+    try {
+      const response = await axiosApi.put('/users/profile', user);
+      dispatch(userUpdateProfileSuccess(response.data));
+      dispatch(userLoginSuccess(response.data));
+
+      localStorage.setItem('userInfo', JSON.stringify(response.data));
+    } catch (e) {
+      dispatch(userUpdateProfileFail(e.response && e.response.data
+        ? e.response.data
+        : e.message));
+    }
+  };
+};
+
+export const getUserList = () => {
+  return async dispatch => {
+    dispatch(userListRequest());
+    try {
+      const response = await axiosApi('/users');
+      dispatch(userListSuccess(response.data));
+    } catch (e) {
+      dispatch(userListFail(e.response && e.response.data
+        ? e.response.data
+        : e.message));
+    }
+  };
+};
+
+export const deleteUser = (id) => {
+  return async dispatch => {
+    dispatch(userDeleteRequest());
+    try {
+      await axiosApi.delete(`/users/${id}`);
+      dispatch(userDeleteSuccess(id));
+    } catch (e) {
+      dispatch(userDeleteFail(e.response && e.response.data
+        ? e.response.data
+        : e.message));
+    }
+  };
+};
+
+export const getUserDetails = (id) => {
+  return async dispatch => {
+    dispatch(userDetailsRequest());
+    try {
+      const response = await axiosApi(`users/${id}`);
+      dispatch(userDetailsSuccess(response.data));
+    } catch (e) {
+      dispatch(userDetailsFail(e.response && e.response.data
+        ? e.response.data
+        : e.message));
+    }
+  };
+};
+
+export const updateUser = (user, id) => {
+  return async dispatch => {
+    dispatch(userUpdateRequest());
+    try {
+      const { data } = await axiosApi.put(`/users/${id}`, user);
+      dispatch(userUpdateSuccess(data));
+    } catch (e) {
+      dispatch(userUpdateFail(e.response && e.response.data
+        ? e.response.data
+        : e.message));
+    }
+  };
+};
+
 
 
--- frontend/src/store/reducers/userReducers.js
+++ frontend/src/store/reducers/userReducers.js
@@ -1,11 +1,21 @@
 import {
+  USER_DELETE_FAIL,
+  USER_DELETE_SUCCESS,
+  USER_DETAILS_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_RESET,
+  USER_DETAILS_SUCCESS, USER_LIST_FAIL, USER_LIST_REQUEST, USER_LIST_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
-  USER_LOGOUT,
+  USER_LOGOUT, USER_PROFILE_FAIL, USER_PROFILE_REQUEST, USER_PROFILE_SUCCESS,
   USER_REGISTER_FAIL,
   USER_REGISTER_REQUEST,
-  USER_REGISTER_SUCCESS,
+  USER_REGISTER_SUCCESS, USER_UPDATE_FAIL,
+  USER_UPDATE_PROFILE_FAIL,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_RESET,
+  USER_UPDATE_PROFILE_SUCCESS, USER_UPDATE_REQUEST, USER_UPDATE_RESET, USER_UPDATE_SUCCESS,
 } from '../constants/userConstants';
 
 const userLoginState = {
@@ -39,13 +49,126 @@ export const userRegisterReducer = (state = userRegisterState, action) => {
     case USER_REGISTER_REQUEST:
       return { ...state, loading: true };
     case USER_REGISTER_SUCCESS:
-      return { ...state, loading: false,};
+      return { ...state, loading: false };
     case USER_REGISTER_FAIL:
       return { ...state, loading: false, error: action.error };
     default:
       return state;
   }
 };
+
+const userProfileState = {
+  loading: false,
+  error: null,
+  user: {},
+};
+
+export const userProfileReducer = (state = userProfileState, action) => {
+  switch (action.type) {
+    case USER_PROFILE_REQUEST:
+      return { ...state, loading: true };
+    case USER_PROFILE_SUCCESS:
+      return { ...state, loading: false, user: action.user };
+    case USER_PROFILE_FAIL:
+      return { ...state, error: action.error };
+    default:
+      return state;
+  }
+}
+
+
+
+const userUpdateProfileState = {
+  loading: false,
+  error: null,
+  success: false,
+  userInfo: {},
+};
+
+export const userUpdateProfileReducer = (state = userUpdateProfileState, action) => {
+  switch (action.type) {
+    case USER_UPDATE_PROFILE_REQUEST:
+      return { ...state, loading: true };
+    case USER_UPDATE_PROFILE_SUCCESS:
+      return { ...state, loading: false, success: true, userInfo: action.updatedUser };
+    case USER_UPDATE_PROFILE_FAIL:
+      return { ...state, loading: false, error: action.error };
+    case USER_UPDATE_PROFILE_RESET:
+      return { ...state, success: false, userInfo: {} };
+    default:
+      return state;
+  }
+};
+
+const userListState = {
+  loading: false,
+  error: null,
+  deleteError: null,
+  success: false,
+  users: [],
+};
+
+export const userListReducer = (state = userListState, action) => {
+  switch (action.type) {
+    case USER_LIST_REQUEST:
+      return { ...state, loading: true };
+    case USER_LIST_SUCCESS:
+      return { ...state, loading: false, users: action.users };
+    case USER_LIST_FAIL:
+      return { ...state, loading: false, error: action.error };
+    case USER_DELETE_SUCCESS:
+      return { ...state, users: state.users.filter(user => user._id !== action.user) };
+    case USER_DELETE_FAIL:
+      return { ...state, deleteError: action.error };
+    default:
+      return state;
+  }
+};
+
+const userDetailsState = {
+  loading: false,
+  error: null,
+  user: {},
+};
+
+export const userDetailsReducer = (state = userDetailsState, action) => {
+  switch (action.type) {
+    case USER_DETAILS_REQUEST:
+      return { ...state, loading: true };
+    case USER_DETAILS_SUCCESS:
+      return { ...state, loading: false, user: action.user };
+    case USER_DETAILS_FAIL:
+      return { ...state, loading: false, error: action.error };
+    case USER_DETAILS_RESET:
+      return { user: {} };
+    default:
+      return state;
+  }
+};
+
+const userUpdateState = {
+  loading: false,
+  error: null,
+  success: false,
+  user: {},
+};
+
+export const userUpdateReducer = (state = userUpdateState, action) => {
+  switch (action.type) {
+    case USER_UPDATE_REQUEST:
+      return { ...state, loading: true };
+    case USER_UPDATE_SUCCESS:
+      return { ...state, loading: false, success: true, user: action.updatedUser };
+    case USER_UPDATE_FAIL:
+      return { ...state, loading: false, error: action.error };
+    case USER_UPDATE_RESET:
+      return { user: {} };
+    default:
+      return state;
+  }
+};
+
+
 
 
 
