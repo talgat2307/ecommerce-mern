a765f2a1b42aba793f15efcbd2688f0ac887a21b
Talgat Muratbekov <talgat2307@gmail.com>
2021-04-12 18:15:02 +0600

    Added Admin dashboard routes & Admin layout

---
+++ frontend/src/components/AdminLayout/AdminLayout.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Button, Container, Grid, List, ListItem, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  gridMenu: {
+    width: '20%',
+  },
+  gridTable: {
+    width: '80%',
+  },
+  list: {
+    width: '100%',
+    padding: 0,
+    marginTop: theme.spacing(2),
+  },
+  listItem: {
+    padding: theme.spacing(1, 0, 1, 0),
+  },
+  btn: {
+    justifyContent: 'start',
+    '&:hover': {
+      backgroundColor: '#e5e5e5',
+    },
+  },
+}));
+
+const AdminLayout = (props) => {
+
+  const classes = useStyles();
+  return (
+    <>
+      <Container maxWidth="lg" className={classes.root}>
+        <Grid item className={classes.gridMenu}>
+          <Typography variant={'h5'}>Admin Dashboard</Typography>
+          <List className={classes.list}>
+            <ListItem className={classes.listItem}>
+              <Button fullWidth className={classes.btn} component={Link} to={'/admin'}>
+                <Typography>
+                  Users
+                </Typography>
+              </Button>
+            </ListItem>
+            <ListItem className={classes.listItem}>
+              <Button fullWidth className={classes.btn} component={Link} to={'/admin/product-list'}>
+                <Typography>
+                  Products
+                </Typography>
+              </Button>
+            </ListItem>
+            <ListItem className={classes.listItem}>
+              <Button fullWidth className={classes.btn} component={Link} to={'/admin/order-list'}>
+                <Typography>
+                  Orders
+                </Typography>
+              </Button>
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item className={classes.gridTable}>
+          {props.children}
+        </Grid>
+      </Container>
+    </>
+  );
+};
+
+export default AdminLayout;
---
+++ frontend/src/containers/AdminDashboard/AdminDashboardRoutes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import AdminLayout from '../../components/AdminLayout/AdminLayout';
+import { Route, Switch } from 'react-router-dom';
+import UserList from './Users/UserList';
+import ProductList from './Products/ProductList';
+import UserEdit from './Users/UserEdit';
+import ProductCreate from './Products/ProductCreate';
+import ProductEdit from './Products/ProductEdit';
+
+const AdminDashboardRoutes = () => {
+  return (
+    <>
+      <AdminLayout>
+        <Switch>
+          <Route path={'/admin'} exact component={UserList}/>
+          <Route path={'/admin/user/:id/edit'} component={UserEdit}/>
+          <Route path={'/admin/product-list'} component={ProductList}/>
+          <Route path={'/admin/product-create'} component={ProductCreate}/>
+          <Route path={'/admin/product/:id/edit'} component={ProductEdit}/>
+         </Switch>
+      </AdminLayout>
+    </>
+  );
+};
+
+export default AdminDashboardRoutes;
