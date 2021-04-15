a8ef006a068b0c71f13c73cc2b040be705eeefa5
Talgat Muratbekov <talgat2307@gmail.com>
2021-04-15 18:28:04 +0600

    Added actions & reducers to get list of orders

--- frontend/src/store/actions/orderActions.js
+++ frontend/src/store/actions/orderActions.js
@@ -1,15 +1,23 @@
 import {
   ORDER_CREATE_FAIL,
   ORDER_CREATE_REQUEST,
-  ORDER_CREATE_SUCCESS,
+  ORDER_CREATE_SUCCESS, ORDER_DELIVER_REQUEST, ORDER_DELIVER_SUCCESS,
   ORDER_DETAILS_FAIL,
   ORDER_DETAILS_REQUEST,
-  ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_SUCCESS, ORDER_LIST_FAIL, ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS,
   ORDER_LIST_USER_FAIL,
   ORDER_LIST_USER_REQUEST,
   ORDER_LIST_USER_SUCCESS,
 } from '../constants/orderConstants';
 import axiosApi from '../../axiosApi';
+
+const orderListUserRequest = () => ({ type: ORDER_LIST_USER_REQUEST });
+const orderListUserSuccess = orders => ({ type: ORDER_LIST_USER_SUCCESS, orders });
+const orderListUserFail = error => ({ type: ORDER_LIST_USER_FAIL, error });
+
+const orderListRequest = () => ({ type: ORDER_LIST_REQUEST });
+const orderListSuccess = orders => ({ type: ORDER_LIST_SUCCESS, orders });
+const orderListFail = error => ({ type: ORDER_LIST_FAIL, error });
 
 const orderCreateRequest = () => ({ type: ORDER_CREATE_REQUEST });
 const orderCreateSuccess = order => ({ type: ORDER_CREATE_SUCCESS, order });
@@ -19,9 +27,38 @@ const orderDetailsRequest = () => ({ type: ORDER_DETAILS_REQUEST });
 const orderDetailsSuccess = order => ({ type: ORDER_DETAILS_SUCCESS, order });
 const orderDetailsFail = error => ({ type: ORDER_DETAILS_FAIL, error });
 
-const orderListUserRequest = () => ({ type: ORDER_LIST_USER_REQUEST });
-const orderListUserSuccess = orders => ({ type: ORDER_LIST_USER_SUCCESS, orders });
-const orderListUserFail = error => ({ type: ORDER_LIST_USER_FAIL, error });
+const orderDeliverRequest = () => ({ type: ORDER_DELIVER_REQUEST });
+const orderDeliverSuccess = order => ({ type: ORDER_DELIVER_SUCCESS, order });
+const orderDeliverFail = error => ({ type: ORDER_DETAILS_FAIL, error });
+
+export const getUserOrders = () => {
+  return async dispatch => {
+
+    dispatch(orderListUserRequest());
+    try {
+      const response = await axiosApi(`/orders`);
+      dispatch(orderListUserSuccess(response.data));
+    } catch (e) {
+      dispatch(orderListUserFail(e.response && e.response.data
+        ? e.response.data
+        : e.message));
+    }
+  };
+};
+
+export const getOrderList = () => {
+  return async dispatch => {
+    dispatch(orderListRequest());
+    try {
+      const response = await axiosApi('/orders/list');
+      dispatch(orderListSuccess(response.data));
+    } catch (e) {
+      dispatch(orderListFail(e.response && e.response.data
+        ? e.response.data
+        : e.message));
+    }
+  };
+};
 
 export const createOrder = (order) => {
   return async dispatch => {
@@ -52,21 +89,21 @@ export const getOrderDetails = (id) => {
   };
 };
 
-
-export const getUserOrders = () => {
+export const deliverOrder = (orderId) => {
   return async dispatch => {
-
-    dispatch(orderListUserRequest());
+    dispatch(orderDeliverRequest());
     try {
-      const response = await axiosApi(`/orders`);
-      dispatch(orderListUserSuccess(response.data));
+      const response = await axiosApi.put(`orders/${orderId}/deliver`);
+      dispatch(orderDeliverSuccess(response.data));
     } catch (e) {
-      dispatch(orderListUserFail(e.response && e.response.data
+      dispatch(orderDeliverFail(e.response && e.response.data
         ? e.response.data
         : e.message));
     }
   };
 };
+
+
 
 
 
--- frontend/src/store/reducers/orderReducers.js
+++ frontend/src/store/reducers/orderReducers.js
@@ -1,10 +1,10 @@
 import {
   ORDER_CREATE_FAIL,
   ORDER_CREATE_REQUEST, ORDER_CREATE_RESET,
-  ORDER_CREATE_SUCCESS,
+  ORDER_CREATE_SUCCESS, ORDER_DELIVER_FAIL, ORDER_DELIVER_REQUEST, ORDER_DELIVER_RESET, ORDER_DELIVER_SUCCESS,
   ORDER_DETAILS_FAIL,
   ORDER_DETAILS_REQUEST,
-  ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_SUCCESS, ORDER_LIST_FAIL, ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS,
   ORDER_LIST_USER_FAIL,
   ORDER_LIST_USER_REQUEST,
   ORDER_LIST_USER_SUCCESS,
@@ -51,14 +51,13 @@ export const orderDetailsReducer = (state = orderDetailsState, action) => {
   }
 };
 
-
-const orderListState = {
+const orderListUserState = {
   loading: true,
   error: null,
   orders: [],
 };
 
-export const orderListReducer = (state = orderListState, action) => {
+export const orderListUserReducer = (state = orderListUserState, action) => {
   switch (action.type) {
     case ORDER_LIST_USER_REQUEST:
       return { ...state, loading: true };
@@ -70,3 +69,44 @@ export const orderListReducer = (state = orderListState, action) => {
       return state;
   }
 };
+
+const orderListState = {
+  loading: false,
+  error: null,
+  orders: [],
+};
+
+export const orderListReducer = (state = orderListState, action) => {
+  switch (action.type) {
+    case ORDER_LIST_REQUEST:
+      return { ...state, loading: true };
+    case ORDER_LIST_SUCCESS:
+      return { ...state, loading: false, orders: action.orders };
+    case ORDER_LIST_FAIL:
+      return { ...state, loading: false, error: action.error };
+    default:
+      return state;
+  }
+};
+
+const orderDeliverState = {
+  loading: false,
+  error: null,
+  success: false,
+};
+
+export const orderDeliverReducer = (state = orderDeliverState, action) => {
+  switch (action.type) {
+    case ORDER_DELIVER_REQUEST:
+      return { ...state, loading: true };
+    case ORDER_DELIVER_SUCCESS:
+      return { ...state, loading: false, success: true };
+    case ORDER_DELIVER_FAIL:
+      return { ...state, loading: false, error: action.error };
+    case ORDER_DELIVER_RESET:
+      return {};
+    default:
+      return state;
+  }
+};
+
