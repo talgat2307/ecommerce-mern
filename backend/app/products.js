f7c15948f2b8ab6d6e4b080e3f3138fd1637e731
Talgat Muratbekov <talgat2307@gmail.com>
2021-04-19 14:03:20 +0600

    Added endpoints to post/delete reviews for product

--- backend/app/products.js
+++ backend/app/products.js
@@ -44,6 +44,34 @@ router.post('/', [auth, permit('admin'), upload.single('image')], async (req, re
   }
 });
 
+router.post('/:id/reviews', [auth], async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    const alreadyReviewed = product.reviews.find(reviewed => reviewed.user.toString() === req.user._id.toString());
+
+    if (alreadyReviewed) {
+      res.status(400).send({ error: 'Product already reviewed' });
+    } else {
+      const review = {
+        name: req.user.name,
+        rating: Number(req.body.rating),
+        comment: req.body.comment,
+        user: req.user._id,
+      };
+
+      product.reviews.push(review);
+      product.numReviews = product.reviews.length;
+
+      product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length;
+
+      await product.save();
+      res.send({ message: 'Review added' });
+    }
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 router.put('/:id', [auth, permit('admin'), upload.single('image')], async (req, res) => {
   try {
     const product = await Product.findById(req.params.id);
@@ -71,6 +99,18 @@ router.delete('/:id', [auth, permit('admin')], async (req, res) => {
     const product = await Product.findById(req.params.id);
     await product.remove();
     res.send({ message: 'Product removed' });
+  } catch (e) {
+    res.status(400).send({ error: 'Product not found' });
+  }
+});
+
+
+router.delete('/:id/reviews', [auth, permit('user', 'admin')], async (req, res) => {
+  try {
+    let query;
+    if (req.query.review) query = { review: req.query.review };
+    await Product.updateOne({ _id: req.params.id },{ '$pull': { 'reviews': { '_id': query.review } } });
+    res.send({ message: 'Review has been successfully deleted' });
   } catch (e) {
     res.status(400).send({ error: 'Product not found' });
   }
